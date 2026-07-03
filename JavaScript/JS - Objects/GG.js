7c747f0658aff5b88dac706d15016b9a3d105d2f
OmEkboteCode <omekbote007@gmail.com>
2026-07-04 02:40:05 +0530

    Complete JS Objects lecture

---
+++ JavaScript/JS - Objects/GG.js
@@ -0,0 +1,28 @@
+const max = prompt("Enter the max number");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+console.log(random);
+
+let guess = prompt("guess the number");
+
+
+while(true){
+    if (guess == "quit"){
+        console.log("user quit")
+        break;
+    }
+    if (guess == random){
+        console.log("You are right! congrats!! random number was", random)
+        break;
+    }else if (guess < random){
+        guess = prompt("Hint: Your guess was too small. Please try again")
+    }else{
+        guess = prompt("Hint: Your guess was too large. Please try again")
+    }
+
+    // else{
+    //     guess = prompt("Your guess was wrong. Please try again")
+    // }
+}
+
---
+++ JavaScript/JS - Objects/app.js
@@ -0,0 +1,69 @@
+// const student= {
+//     name: "Sunny",
+//     age: 19,
+//     element: "Shadow"
+// }
+
+// const post = {
+//     username: "Steve",
+//     content: "This is my first post",
+//     likes: 158,
+//     reposts: 5,
+//     tags: ["apple", "Iphone"]
+// }
+
+// post["likes"];
+
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     true: "c",
+//     null: "d",
+//     undefined: "e"
+// }
+
+// const student= {
+//     name: "Sunny",
+//     age: 19,
+//     element: "Shadow",
+//     mark: "Divine",
+//     fragments: 556,
+//     soulstones: 196
+// }
+
+// const awakeners = {
+//     sunny: {
+//         age: 19,
+//         Core: "Shadow",
+//         aspectRank: "Divine",
+//         Aspect: "Shadow Slave"
+//     },
+//     nephis: {
+//         age: 19,
+//         Core: "Soul Flame",
+//         aspectRank: "Divine",
+//         Aspect: "Light Bringer"
+    
+//     }
+// }
+
+const awakeners = [
+    {
+        name: "Sunny",
+        age: 19,
+        Core: "Shadow",
+        aspectRank: "Divine",
+        Aspect: "Shadow Slave"
+    },
+    {
+        name: "Nephis",
+        age: 19,
+        Core: "Soul Flame",
+        aspectRank: "Divine",
+        Aspect: "Light Bringer"
+    
+    }
+]
+
+let num = Math.random();

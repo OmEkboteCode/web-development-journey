dea18e1c462e2e589c5c65af469bb2af9ad2ffbe
OmEkboteCode <omekbote007@gmail.com>
2026-07-04 09:01:30 +0530

    Build application skeleton

---
+++ Projects/JavaScript/Shopping Cart/v1/app.js
@@ -0,0 +1,29 @@
+let cart = [];
+let option;
+
+while(true){
+        console.log("===== SHOPPING CART =====");
+        console.log("1. Add Product");
+        console.log("2. View Cart");
+        console.log("3. Remove Product");
+        console.log("4. Checkout");
+        console.log("5. Exit");
+
+        option = prompt("Choose an option")
+        option = option.trim();
+        option = option.toLowerCase();
+        if (option === "exit"){
+                console.log("Thank You For Visiting Us");
+                break;
+        } else{
+                console.log("Feature not implemented yet.")
+        }
+}
+
+
+
+
+
+
+//ROUGH WORK
+

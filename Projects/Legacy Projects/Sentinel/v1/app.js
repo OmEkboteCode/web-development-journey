c1b21e280bbf0fa50c54e57e612eac914c54f9ad
OmEkboteCode <omekbote007@gmail.com>
2026-07-06 19:09:31 +0530

    Initialize Sentinel Prototype 0 skeleton

---
+++ Projects/Legacy Projects/Sentinel/v1/app.js
@@ -0,0 +1,22 @@
+let submissions = [];
+
+
+while(true){
+        console.log("======Sentinel Prototype 0======");
+        console.log("Submit Project");
+        console.log("View Submissions");
+        console.log("Review Submission");
+        console.log("Exit");
+        let option = prompt("Enter your option");
+        option = option.trim();
+        option = option.toLowerCase();
+
+
+        if(option === "exit"){
+                console.log("Trust Begins With Truth.")
+                break;
+        }
+        else{
+                console.log("Feature not implemented yet.")
+        }
+}

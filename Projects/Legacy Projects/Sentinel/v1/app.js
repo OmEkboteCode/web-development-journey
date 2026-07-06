27af2694eb7750ff66197188c33425e6c1c3ed18
OmEkboteCode <omekbote007@gmail.com>
2026-07-06 20:33:07 +0530

    Implement Project Submission Feature

--- Projects/Legacy Projects/Sentinel/v1/app.js
+++ Projects/Legacy Projects/Sentinel/v1/app.js
@@ -16,6 +16,48 @@ while(true){
                 console.log("Trust Begins With Truth.")
                 break;
         }
+        else if(option === "submit project"){
+                let creatorName = prompt("Enter Your Creator Name");
+                creatorName = creatorName.trim();
+                creatorName = creatorName.toUpperCase();
+                while(creatorName === ""){
+                        console.log("Invalid Creator Name. Please try again");
+                        creatorName = prompt("Enter Your Creator Name");
+                        creatorName = creatorName.trim();
+                        creatorName = creatorName.toUpperCase();
+                }
+                let title =  prompt("Work Title");
+                title = title.trim();
+                title = title.toUpperCase();
+                while(title === ""){
+                        console.log("Invalid Title. Please try again");
+                        title =  prompt("Enter Valid Title");
+                        title = title.trim();
+                        title = title.toUpperCase();
+                }
+                let description =  prompt("Description");
+                description = description.trim();
+                // description = description.toUpperCase();
+                while(description === ""){
+                        console.log("Invalid Description. Please try again");
+                        description =  prompt("Please Enter Valid Description");
+                        description = description.trim();
+                        // description = description.toUpperCase();
+                }
+                let submission = {};
+                submission.creatorName = creatorName;
+                submission.title = title;
+                submission.description = description;
+                submission.status = "Submitted";
+                submissions.push(submission)
+                console.log("Project submitted successfully.")
+                console.log("Submission Preview");
+                console.log("Creator: ", submission.creatorName);
+                console.log("Title: ", submission.title);
+                console.log("Description: ", submissiondescription);
+                console.log("Status:", submission.status);
+                console.log("Waiting for review...")
+        }
         else{
                 console.log("Feature not implemented yet.")
         }

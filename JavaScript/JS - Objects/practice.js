2dca5f562da81cd1ac4a8d23d598ad52646441c0
OmEkboteCode <omekbote007@gmail.com>
2026-07-08 22:33:42 +0530

    Practice Objects

--- JavaScript/JS - Objects/practice.js
+++ JavaScript/JS - Objects/practice.js
@@ -31,7 +31,6 @@
 // delete book.available;
 // console.log(book);
 
-
 // const student = {
 //     name: "Glacier",
 //     age: 19,
@@ -41,7 +40,6 @@
 //         chemistry: 95
 //     }
 // }
-
 
 // console.log(student.marks.physics)
 // student.marks.chemistry = 90;
@@ -74,13 +72,11 @@
 //         console.log(`${library[i].title} - ${library[i].pages}`)
 // }
 
-
 //P1C5
 
 // let random = Math.floor(Math.random() * 6) + 1;
 // console.log(`You rolled: ${random}`);
 // let count = 0;
-
 
 // for (let i = 1; i <= 10; i++){
 //         random = Math.floor(Math.random() * 6) + 1;
@@ -107,9 +103,9 @@
 
 // console.log("Students with 60+ marks:")
 // for (let i = 0; i<students.length; i++){
-        
+
 //         if(students[i].marks >= 60){
-                
+
 //                 console.log(students[i].name)
 //         }
 //         if(students[i].marks >= 40){
@@ -124,8 +120,6 @@
 // console.log("Passed Students: ", passed);
 // console.log("Topper: ", topper, "Marks: ", largest);
 
-
-
 //P2C2
 // const users = [
 //     { username: "Glacier", active: true, score: 150 },
@@ -135,41 +129,35 @@
 //     { username: "Luna", active: false, score: 95 }
 // ];
 
-
-
 // for (let i = 0; i<users.length; i++){
 //         if(users[i].active === true){
 //                 console.log(users[i].username);
 //                 users[i].score += 50;
 //         } else{
 //                 users[i].score -= 20;
-        //}
+//}
 
-        
 // }
 
 // console.log(users)
 
-
-
 //P2C3
 
-
 const orders = [
-    { customer: "Glacier", item: " Laptop ", quantity: 1, price: 60000 },
-    { customer: "Nova", item: "mouse", quantity: 2, price: 800 },
-    { customer: "Arya", item: " KEYBOARD ", quantity: 1, price: 1500 },
-    { customer: "Kai", item: "monitor", quantity: 3, price: 12000 }
+  { customer: "Glacier", item: " Laptop ", quantity: 1, price: 60000 },
+  { customer: "Nova", item: "mouse", quantity: 2, price: 800 },
+  { customer: "Arya", item: " KEYBOARD ", quantity: 1, price: 1500 },
+  { customer: "Kai", item: "monitor", quantity: 3, price: 12000 },
 ];
 
 let grandTotal = 0;
-for (let i = 0; i<orders.length; i++){
-        orders[i].item = orders[i].item.trim();
-        orders[i].item = orders[i].item.toUpperCase();
-        orders[i].total = orders[i].quantity * orders[i].price;
-        console.log(orders[i].customer.toUpperCase(), "ordered", orders[i].item);
-        console.log(`Total: ₹${orders[i].total}`);
-        grandTotal += orders[i].total
+for (let i = 0; i < orders.length; i++) {
+  orders[i].item = orders[i].item.trim();
+  orders[i].item = orders[i].item.toUpperCase();
+  orders[i].total = orders[i].quantity * orders[i].price;
+  console.log(orders[i].customer.toUpperCase(), "ordered", orders[i].item);
+  console.log(`Total: ₹${orders[i].total}`);
+  grandTotal += orders[i].total;
 }
-console.log(orders)
-console.log(grandTotal)
+console.log(orders);
+console.log(grandTotal);

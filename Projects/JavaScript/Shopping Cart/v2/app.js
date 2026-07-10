19e3478128e55fcadb65b96678d87fcdb7232912
OmEkboteCode <omekbote007@gmail.com>
2026-07-10 15:35:44 +0530

    Implement shopping cart menu system

--- Projects/JavaScript/Shopping Cart/v2/app.js
+++ Projects/JavaScript/Shopping Cart/v2/app.js
@@ -1,34 +1,38 @@
-const formatText = (text) =>{
-   return text.trim().toLowerCase();
-}
+// const formatText = (text) => {
+//   return text.trim().toLowerCase();
+// };
 
-let displayMenu = () =>{
-    console.log(
-        `=====Shopping Cart V2======
+let displayMenu = () => {
+  console.log(
+    `=====Shopping Cart V2======
     1. Add Product
     2. View Cart
     3. Remove Product
     4. Checkout
-    5. Exit`
-    )
-    let option = prompt("Enter your option");
-    option = formatText(option)
-    return option
-}
+    5. Exit`,
+  );
+  let option = parseInt(prompt("Choose an option:"));
+  return option;
+};
 
+let running = true;
 
-let running = true
-
-while(running){
-    switch(displayMenu()){
-        case "quit":
-            console.log("Thank You for Visiting Us");
-            running = false;
-            break;
-        case: "Add Product"
-        case "":
-            console.log("Feature not implemented yet.");
-            break;
-    }
-    
+while (running) {
+  switch (displayMenu()) {
+    case 5:
+      console.log("Thank You for Visiting Us");
+      running = false;
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    default:
+      console.log("Invalid Option. Please try again");
+      break;
+  }
 }

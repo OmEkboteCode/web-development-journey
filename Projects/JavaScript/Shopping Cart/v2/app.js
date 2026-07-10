bda5b3905686efca022ba595751fc88d104e1158
OmEkboteCode <omekbote007@gmail.com>
2026-07-10 17:36:41 +0530

    Implement add product functionality

--- Projects/JavaScript/Shopping Cart/v2/app.js
+++ Projects/JavaScript/Shopping Cart/v2/app.js
@@ -1,6 +1,9 @@
-// const formatText = (text) => {
-//   return text.trim().toLowerCase();
-// };
+let cart = [];
+
+
+const formatText = (text) => {
+  return text.trim().toUpperCase();
+};
 
 let displayMenu = () => {
   console.log(
@@ -15,6 +18,33 @@ let displayMenu = () => {
   return option;
 };
 
+//Feature Functions
+function addProduct(){
+    let productName = prompt("Enter Product Name");
+        productName = formatText(productName);
+        while (productName === ""){
+            console.log("Invalid product name. Please try again.")
+            productName = prompt("Enter Product Name");
+            productName = formatText(productName);
+        }
+    let stars = parseInt(prompt("Enter the Quality Stars 1-5"));
+        while(stars > 5 || stars < 1){
+            console.log("Invalid Star numbers. Please try again");
+            stars = parseInt(prompt("Enter the Quality Stars 1-5"));
+            
+        }
+        let price = stars * 100;
+        cart.push(
+            {
+                name: productName,
+                stars: stars,
+                price: price
+            }
+        );
+        console.log(`${productName} added successfully`)
+}
+
+
 let running = true;
 
 while (running) {
@@ -24,6 +54,7 @@ while (running) {
       running = false;
       break;
     case 1:
+        addProduct()
       break;
     case 2:
       break;
